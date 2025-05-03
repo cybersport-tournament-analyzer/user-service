874c3dd52db47ecba9c300b693ce4386592aeef9
yofujitsu <yofujitsuuu@gmail.com>
2025-05-03 16:06:09 +0300

    new front ip 2

--- src/main/java/com/vkr/user_service/config/CorsConfig.java
+++ src/main/java/com/vkr/user_service/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.vkr.user_service.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -12,12 +13,15 @@ import java.util.List;
 @Configuration
 public class CorsConfig {
 
+    @Value("${cors.allowed-origins.local}")
+    private String allowedOrigins;
+
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of("http://localhost:4200", "http://176.98.178.99:4200"));
+        config.setAllowedOrigins(List.of(allowedOrigins, "http://176.98.178.99:4200", "http://localhost:4200"));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         source.registerCorsConfiguration("/**", config);
