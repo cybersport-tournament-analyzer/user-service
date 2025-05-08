67bb40d3d5bedefab93d4ae352efc915e1438d69
yofujitsu <yofujitsuuu@gmail.com>
2025-05-08 20:50:46 +0300

    cors

--- src/main/java/com/vkr/user_service/config/CorsConfig.java
+++ src/main/java/com/vkr/user_service/config/CorsConfig.java
@@ -1,10 +1,8 @@
 package com.vkr.user_service.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
@@ -14,15 +12,12 @@ import java.util.List;
 @Configuration
 public class CorsConfig {
 
-    @Value("${cors.allowed-origins.local}")
-    private String allowedOrigins;
-
     @Bean
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of(allowedOrigins, "http://176.98.178.99:4200", "http://localhost:4200"));
+        config.setAllowedOrigins(List.of("http://176.98.178.99:4200", "http://localhost:4200"));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         source.registerCorsConfiguration("/**", config);
